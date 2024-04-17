60d6e0618404cd103f20dc0074376e98d9fa2dfd
Kinsammy <fanusamuel@gmail.com>
2024-04-17 19:34:33 +0100

    Onboarding  Screen updated

--- lib/src/constants/text_strings.dart
+++ lib/src/constants/text_strings.dart
@@ -3,4 +3,12 @@ const String tAppTagLine = "Learn To Code. \nFree For Everyone!";
 
 
 //Onboarding Texts
-const String tOnboardingTitle1 = "Build Awesome"
+const String tOnboardingTitle1 = "Build Awesome Apps";
+const String tOnboardingTitle2 = "Learn from Youtube";
+const String tOnboardingTitle3 = "Get Code & Resources";
+const String tOnboardingSubTitle1 = "Let's start your journey with use on this amazing and easy way.";
+const String tOnboardingSubTitle2 = "Get video Tutorials of each topic to learn things easily";
+const String tOnboardingSubTitle3 = "Save time by just copy pasting complete apps learn from the latest version";
+const String tOnboardingCounter1 = "1/3";
+const String tOnboardingCounter2 = "2/3";
+const String tOnboardingCounter3 = "3/3";
