d3418bb954e241dfd353dfb52711e27e26f815bc
Kinsammy <fanusamuel@gmail.com>
2024-04-19 09:46:27 +0100

    Welcome Screen in progress ...

--- lib/src/constants/image_strings.dart
+++ lib/src/constants/image_strings.dart
@@ -2,6 +2,7 @@
 
 // const String topIcon = "assets/images/gold.png";
 const String revoSplashImage = "assets/images/flower.png";
-const onboardingImage1 = "assets/images/gold.png";
-const onboardingImage2 = "assets/images/top.png";
-const banner = "assets/images/banner.png";
+const String onboardingImage1 = "assets/images/gold.png";
+const String onboardingImage2 = "assets/images/top.png";
+const String banner = "assets/images/banner.png";
+const String author = "assets/images/auth.png";
